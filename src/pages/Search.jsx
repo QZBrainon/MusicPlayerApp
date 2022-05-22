ebb8f4106b7c51fad3817816ec29a74ff84f8161
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-05-22 00:59:02 -0300

    Req 6 OK

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../services/components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 export default class Search extends Component {
   state={
     nameOfSearch: '',
+    savedName: '',
     isBtnDisabled: true,
     minNameLength: 2,
+    loading: false,
+    requestResults: [],
+    showResults: false,
   }
 
   typeHandler = ({ target }) => {
@@ -13,6 +20,9 @@ export default class Search extends Component {
     this.setState({
       [name]: value,
     }, this.verifyBtn);
+    this.setState({
+      savedName: value,
+    });
   }
 
   verifyBtn = () => {
@@ -28,32 +38,84 @@ export default class Search extends Component {
     }
   }
 
+  onSearchBtnClick = async () => {
+    const { nameOfSearch } = this.state;
+    this.setState({
+      nameOfSearch: '',
+      loading: true,
+    });
+    const request = await searchAlbumsAPI(nameOfSearch);
+    console.log(request);
+    this.setState({
+      loading: false,
+      requestResults: request,
+      showResults: true,
+    });
+  }
+
+  // verifyResults = () => {
+  //   const {requestResults} = this.state;
+  //   if (requestResults !== undefined){
+
+  //   }
+  // }
+
   render() {
-    const { isBtnDisabled, nameOfSearch } = this.state;
+    const { isBtnDisabled,
+      nameOfSearch,
+      loading,
+      requestResults,
+      savedName,
+      showResults } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
-        <form>
-          Nome
-          <input
-            type="text"
-            value={ nameOfSearch }
-            name="nameOfSearch"
-            onChange={ this.typeHandler }
-            placeholder=" Banda ou artista"
-            data-testid="search-artist-input"
-          />
-          <button
-            disabled={ isBtnDisabled }
-            data-testid="search-artist-button"
-            type="button"
-          >
-            Pesquisar
+        {loading ? <Loading />
+          : (
+            <form>
+              Nome
+              <input
+                type="text"
+                value={ nameOfSearch }
+                name="nameOfSearch"
+                onChange={ this.typeHandler }
+                placeholder=" Banda ou artista"
+                data-testid="search-artist-input"
+              />
+              <button
+                disabled={ isBtnDisabled }
+                data-testid="search-artist-button"
+                type="button"
+                onClick={ this.onSearchBtnClick }
+              >
+                Pesquisar
 
-          </button>
+              </button>
 
-        </form>
+            </form>
+          )}
+        {
+          showResults ? <h2>{`Resultado de álbuns de: ${savedName}`}</h2>
+            : null
+
+        }
+        <div>
+
+          {
+            !requestResults.length ? <h2>Nenhum álbum foi encontrado</h2>
+              : requestResults.map((result) => (
+                <Link
+                  key={ result.artistId }
+                  data-testid={ `link-to-album-${result.collectionId}` }
+                  to={ `/album/${result.collectionId}` }
+                >
+                  {result.collectionName}
+
+                </Link>))
+          }
+
+        </div>
       </div>
     );
   }
